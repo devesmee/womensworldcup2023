0bf67c1c65536839531cbe5e1529e00e9378222f
Esmee <esmee.kluijtmans@gmail.com>
2023-07-05 15:17:24 +0200

    Update navigation title colour & add reset map region button

--- WomensWorldCup2023/WomensWorldCup2023/Views/Stadiums tab/StadiumMapView.swift
+++ WomensWorldCup2023/WomensWorldCup2023/Views/Stadiums tab/StadiumMapView.swift
@@ -15,16 +15,27 @@ struct StadiumMapView: View {
 
     var body: some View {
         NavigationStack(path: $path) {
-            Map(coordinateRegion: $mapRegion, annotationItems: stadiums) { stadium in
-                MapAnnotation(coordinate: stadium.coordinate) {
-                    NavigationLink(value: stadium) {
-                        Image("logo_rounded")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 44, height: 44)
-                            .clipShape(Circle())
+            ZStack(alignment: .topTrailing) {
+                Map(coordinateRegion: $mapRegion, annotationItems: stadiums) { stadium in
+                    MapAnnotation(coordinate: stadium.coordinate) {
+                        NavigationLink(value: stadium) {
+                            Image("logo_rounded")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 44, height: 44)
+                                .clipShape(Circle())
+                        }
                     }
                 }
+                Button {
+                    self.resetMapRegion()
+                } label: {
+                    Image(systemName: "arrow.counterclockwise.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                .padding(10)
+
             }
             .navigationDestination(for: Stadium.self) { stadium in
                 StadiumDetailView(stadium: stadium)
@@ -35,6 +46,15 @@ struct StadiumMapView: View {
     }
 
     init() {
+        self.setNavigationTitleColor()
+        self.loadStadiumData()
+    }
+
+    private func setNavigationTitleColor() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color("Blue"))]
+    }
+
+    private mutating func loadStadiumData() {
         if let path = Bundle.main.path(forResource: "stadiums", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path))
@@ -46,6 +66,10 @@ struct StadiumMapView: View {
                 print(error.localizedDescription)
             }
         }
+    }
+
+    private func resetMapRegion() {
+        mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -30, longitude: 146), span: MKCoordinateSpan(latitudeDelta: 70, longitudeDelta: 70))
     }
 }
 
