7ead6e21650c0eef792eb4bfb1ee633ee747d625
devesmee <esmee.kluijtmans@gmail.com>
2023-09-29 14:58:19 +0200

    Request camera permission

--- WomensWorldCup2023/WomensWorldCup2023/Views/AR tab/ARView.swift
+++ WomensWorldCup2023/WomensWorldCup2023/Views/AR tab/ARView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ARView: View {
+    private var isAuthorized: Bool {
+        get async {
+            let status = AVCaptureDevice.authorizationStatus(for: .video)
+
+            var isAuthorized = status == .authorized
+
+            if status == .notDetermined {
+                isAuthorized = await AVCaptureDevice.requestAccess(for: .video)
+            }
+
+            return isAuthorized
+        }
+    }
+
     var body: some View {
         ZStack {
             Color("Yellow").ignoresSafeArea()
             Text("AR")
         }
+        .task {
+            await self.setUpCamera()
+        }
+    }
+
+    private func setUpCamera() async {
+        guard await isAuthorized else {
+            print("User has not authorized camera usage")
+            return
+        }
+        print("User has authorized camera usage")
+        // TODO: implement showing camera view
     }
 }
 
